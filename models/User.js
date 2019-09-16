854adb8179873e19c88ad49aae10c7ccbec38e86
timdavidcole <timdavidcole@gmail.com>
2019-09-16 19:32:30 +0100

    added user model

--- models/User.js
+++ models/User.js
@@ -1,14 +1,18 @@
 var mongoose = require('mongoose');
+var uniqueValidator = require('mongoose-unique-validator');
+var crypto = require('crypto');
 
 var UserSchema = new mongoose.Schema({
-    username: {type: String, lowercase: true, required: [true, "can't be blank"], match: [/^[a-zA-Z0-9]+$/, 'is invalid'], index: true},
-    name: String,
-    email: {type: String, lowercase: true, required: [true, "can't be blank"], match: [/\S+@\S+\.\S+/, 'is invalid'], index: true},
-    address: String,
-    dob: String,
-    image: String,
-    hash: String,
-    salt: String
+  username: {type: String, lowercase: true, unique: true, required: [true, "can't be blank"], match: [/^[a-zA-Z0-9]+$/, 'is invalid'], index: true},
+  name: String,
+  email: {type: String, lowercase: true, unique: true, required: [true, "can't be blank"], match: [/\S+@\S+\.\S+/, 'is invalid'], index: true},
+  address: String,
+  dob: String,
+  image: String,
+  hash: String,
+  salt: String
 }, {timestamps: true});
+
+UserSchema.plugin(uniqueValidator, {message: 'is already taken.'});
 
 mongoose.model('User', UserSchema);
