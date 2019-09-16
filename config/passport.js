05427a13e8a767a1635d8febdbac8a02411c47e7
timdavidcole <timdavidcole@gmail.com>
2019-09-16 21:37:30 +0100

    added User Routes

--- config/passport.js
+++ config/passport.js
@@ -7,9 +7,15 @@ passport.use(new LocalStrategy({
   usernameField: 'user[email]',
   passwordField: 'user[password]'
 }, function(email, password, done) {
-  User.findOne({email: email}).then(function(user){
-    if(!user || !user.validPassword(password)){
-      return done(null, false, {errors: {'email or password': 'is invalid'}});
+  User.findOne({
+    email: email
+  }).then(function(user) {
+    if (!user || !user.validPassword(password)) {
+      return done(null, false, {
+        errors: {
+          'email or password': 'is invalid'
+        }
+      });
     }
 
     return done(null, user);
