d13ec201c3e26ac91fb3592309ef0cf28efce563
Timdavidcole <timdavidcole@gmail.com>
2020-03-28 01:05:46 +0000

    Removed a comment.

--- models/Notice.js
+++ models/Notice.js
@@ -1,6 +1,6 @@
 var mongoose = require("mongoose");
 var uniqueValidator = require("mongoose-unique-validator");
-var slug = require("slug"); // package we'll use to auto create URL slugs
+var slug = require("slug");
 var User = mongoose.model("User");
 
 var NoticeSchema = new mongoose.Schema(
